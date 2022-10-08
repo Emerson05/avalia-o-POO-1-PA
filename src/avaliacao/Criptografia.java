39cba3cff8e0e5f8e255def8259a0210fb5d8cd3
Emerson05 <61097794+Emerson05@users.noreply.github.com>
2022-10-08 00:29:37 -0300

    Update Criptografia.java

--- src/avaliacao/Criptografia.java
+++ src/avaliacao/Criptografia.java
@@ -1,34 +1,35 @@
 package avaliacao;
-
-
-
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-
-public class Criptografia {
-	
-	public static String encriptografar(String senha) {
-		 senha = "";
-		
-		
-		try {
-			KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-			SecretKey secretkey = keyGenerator.generateKey();
-			Cipher cipher;
-			cipher = Cipher.getInstance("AES");
-			cipher.init(cipher.ENCRYPT_MODE,secretkey);
-			byte[] criptrogafia = cipher.doFinal(senha.getBytes());
-			System.out.println("Senha criptografada: " +criptrogafia);
-		}
-		
-		catch(Exception e) {
-			
-		}
-		
-		
-		return senha;
-	}
-	
-	
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+ 
+public class Criptografia{
+  private static final String SECRET_KEY = "secreto";
+  private static final String SALT = "sheyyy!";
+ 
+  public static String encrypt(String senha) {
+    try {
+      byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      IvParameterSpec ivspec = new IvParameterSpec(iv);
+ 
+      SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+      KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
+      SecretKey tmp = factory.generateSecret(spec);
+      SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+ 
+      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+      cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+      return Base64.getEncoder()
+          .encodeToString(cipher.doFinal(senha.getBytes(StandardCharsets.UTF_8)));
+    } catch (Exception e) {
+      System.out.println("Error while encrypting: " + e.toString());
+    }
+    return null;
+  }
 }
