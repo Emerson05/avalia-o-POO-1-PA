60834e20ba77f2ea1ff83712e8fee97ae2357cfd
Emerson05 <61097794+Emerson05@users.noreply.github.com>
2022-10-08 22:26:33 -0300

    Create Conexao.java

---
+++ src/avaliacao/Conexao.java
@@ -0,0 +1,22 @@
+package avaliacao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+
+	private static final String USUARIO = "root";
+    private static final String SENHA = "mjejiml";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/Estoque";
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+
+
+	public static Connection abrir() throws Exception {
+
+		Class.forName(DRIVER);
+		Connection conn = DriverManager.getConnection(URL, USUARIO, SENHA);
+
+		return conn;
+	}
+}
